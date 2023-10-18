783175a5ac3cf9d4bf50245dcdeed4d04ad78e0e
Akash Singhal <84220003+AS200211@users.noreply.github.com>
2023-10-18 13:35:50 +0530

    2nd

---
+++ firstnnat.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter a number where you want to print natural number" << endl;
+    cin >> n;
+    int i=0;
+    while (i<=n){
+        cout << i <<" ";
+        i++;
+    }
+
+    return 0;
+}
