2db705e6e24930ce1b31ccccf002eee952e57012
Akash Singhal <84220003+AS200211@users.noreply.github.com>
2023-10-20 06:05:56 +0000

    String 1st program to take input

---
+++ pos/str1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string s = "Prepinsta";
+    cout << s << endl;
+    string str;
+    cout << "Enter a string: ";
+    getline(cin, str);
+    cout << str;
+    return 0;
+}
