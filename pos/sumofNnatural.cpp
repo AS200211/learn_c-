d62b4398664ad93011897aa8a2f60df364b9b92d
Akash Singhal <84220003+AS200211@users.noreply.github.com>
2023-10-23 16:18:47 +0000

    sum of first n Natural numbers

---
+++ pos/sumofNnatural.cpp
@@ -0,0 +1,23 @@
+// sum of first n Natural numbers 
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,s=0;
+    cout << "Enter the value of n: " ;
+    cin >> n;
+    
+    
+    
+    for(int i=1;i<=n;i++){
+        s+=i;
+    }
+    cout << s << endl;
+    
+    
+
+    return 0;
+}
