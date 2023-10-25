dda943f3bfae2150c519ed258b7987765b2adf09
Akash Singhal <84220003+AS200211@users.noreply.github.com>
2023-10-25 05:45:34 +0000

    changed

--- pos/hcfGcd.cpp
+++ pos/hcfGcd.cpp
@@ -14,7 +14,7 @@ int main()
     cin >> num1 >> num2;
     int hcf=1;
     for (int i=1;i<=num1/2&&i<=num2/2;i++){
-        if (num1%i==0 && num2%2==0){
+        if (num1%i==0 && num2%i==0){
             hcf = i;
         }
     }
